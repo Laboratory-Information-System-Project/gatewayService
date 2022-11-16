fe94e8f6a75a3a1d68428d47203301533eb85ddc
liverbird <ehdgns07@gmail.com>
2022-11-16 20:59:38 +0900

    CORS filter config apply

---
+++ src/main/java/com/douzone/gatewayservice/filter/CustomFilter.java
@@ -0,0 +1,41 @@
+package com.douzone.gatewayservice.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+@Order(value = Integer.MIN_VALUE)
+public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
+    public CustomFilter(){
+        super(Config.class);
+    }
+
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // Custom pre filter
+        log.info("test");
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            HttpHeaders test = response.getHeaders();
+            log.info("header: {}",test);
+
+            return chain.filter(exchange).then(Mono.fromRunnable(()-> {
+                log.info("Custom POST filter: response code ->{}" , response.getStatusCode());
+            }));
+        });
+    }
+
+    public static class Config {}
+
+}
