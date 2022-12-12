3863b7c414e1c68667b74edb121f0ede99679029
liverbird <ehdgns07@gmail.com>
2022-12-12 10:31:01 +0900

    deploy test

--- src/main/java/com/douzone/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/douzone/gatewayservice/GatewayServiceApplication.java
@@ -11,7 +11,7 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
 
-        
+
     }
 
 }
