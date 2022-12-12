00b4af247d8544f549a5fdda9ec583d557c3639a
liverbird <ehdgns07@gmail.com>
2022-12-12 10:15:06 +0900

    push

--- src/main/java/com/douzone/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/douzone/gatewayservice/GatewayServiceApplication.java
@@ -10,6 +10,8 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+
+        
     }
 
 }
